9fadd20665739b339d6034d943316a23bf34778b
ct-KlimSaenko <klimentsisaenko@coherentsolutions.com>
2024-03-28 12:15:42 +0100

    base url changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,24 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
-export default defineConfig({
-  // base: process.env.VITE_BASE_URL,
-  base: '/car-service/',
-  plugins: [
-    vue(),
-    vueJsx()
-  ],
-  resolve: {
-    alias: [
-      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
-      { find: '@logos', replacement: fileURLToPath(new URL('./public/logos', import.meta.url)) },
-      { find: '@config', replacement: fileURLToPath(new URL('./config', import.meta.url)) }
-    ]
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    base: env.VITE_BASE_URL,
+    plugins: [
+      vue(),
+      vueJsx()
+    ],
+    resolve: {
+      alias: [
+        { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+        { find: '@logos', replacement: fileURLToPath(new URL('./public/logos', import.meta.url)) },
+        { find: '@config', replacement: fileURLToPath(new URL('./config', import.meta.url)) }
+      ]
+    }
   }
 });
